2d8776c7e6095e35a1cb466c8cc87d0a3034b354
rares4racu <rares4racu@gmail.com>
2026-01-07 13:54:03 +0200

    Implementare baza de date

--- ProiectPythonSOTW/Proiect.py
+++ ProiectPythonSOTW/Proiect.py
@@ -1,12 +1,15 @@
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
 import re
+import sqlite3
+from urllib.parse import quote
 
 
 def clean_text(text):
-    text = re.sub(r'\[\w+', '', text)
-    text = text.replace("\xa0", " ")
-    return " ".join(text.split()).strip()
+    if not text:
+        return None
+    text = re.sub(r"\[[^\]]*\]", "", text).strip()
+    return text
 
 
 def find_field(ib, name):
@@ -26,24 +29,43 @@ def find_field(ib, name):
 
 
 def remove_unwanted(text):
+    if not text:
+        return None
     return text.split(" ")[0]
 
 
 def complete_density(infobox):
     density_raw = find_field(infobox, ["density"])
-    density = remove_unwanted(density_raw)
-    if "/km" in density:
-        density_final = density + "^2"
-    else:
-        density_final = density + "/km^2"
-    return density_final
+    if not density_raw:
+        return None
+    density_raw = density_raw.replace(",", "")
+    match = re.search(r"\d+(\.\d+)?", density_raw)
+    return float(match.group()) if match else None
 
 
 def complete_area(infobox):
     area_raw = find_field(infobox, ["total"])
-    area = remove_unwanted(area_raw)
-    area_final = area + " km^2"
-    return area_final
+    if not area_raw:
+        return None
+    area_raw = area_raw.replace(",", "")
+    match = re.search(r"\d+(\.\d+)?", area_raw)
+    return float(match.group()) if match else None
+
+def get_all_countries(soup_borders):
+    table = soup_borders.find("table", class_="wikitable sortable")
+    countries = []
+    for row in table.find_all("tr"):
+        tds = row.find_all("td")
+        if len(tds) < 6:
+            continue
+
+        country_a = tds[0].find("a")
+        if not country_a:
+            continue
+        name = country_a.get_text(strip=True)
+        countries.append(name)
+
+    return countries
 
 
 def get_neighbours(country_name, soup_borders):
@@ -70,45 +92,98 @@ def get_neighbours(country_name, soup_borders):
 
     return None
 
+def init_db():
+    conn = sqlite3.connect('countries.db')
+    cursor = conn.cursor()
+    cursor.execute("""
+            CREATE TABLE IF NOT EXISTS countries (
+                name TEXT PRIMARY KEY,
+                capital TEXT,
+                population INTEGER,
+                density REAL,
+                area REAL,
+                language TEXT,
+                time_zone TEXT,
+                government TEXT,
+                neighbours TEXT
+            );
+        """)
+    conn.commit()
+    return conn
 
-def scrape(url, url_borders):
-    # Site-ul tarii
-    req_country = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-    webpage_country = urlopen(req_country).read()
-    soup_country = BeautifulSoup(webpage_country, 'html.parser')
-    country_name = soup_country.find("h1", id="firstHeading").text
-    infobox_country = soup_country.find("table", class_="infobox")
-    capital_raw = find_field(infobox_country, ["capital"])
-    population_raw = find_field(infobox_country, ["estimate"])
-    language_raw = find_field(infobox_country, ["language"])
-    # Site-ul granitelor
+
+failed_countries = []
+def scrape(country, soup_borders, conn):
+    try:
+        url = f"https://en.wikipedia.org/wiki/" + quote(country)
+        req_country = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+        webpage_country = urlopen(req_country).read()
+        soup_country = BeautifulSoup(webpage_country, 'html.parser')
+        country_name = soup_country.find("h1", id="firstHeading").text
+        infobox_country = soup_country.find("table", class_="infobox")
+        if not infobox_country:
+            raise ValueError("Infobox not found")
+        capital_raw = find_field(infobox_country, ["capital"])
+        population_raw = find_field(infobox_country, ["estimate"],)
+        population_text = remove_unwanted(population_raw)
+        if population_text:
+            match = re.search(r'\d+', population_text.replace(",", ""))
+            population = int(match.group()) if match else None
+        else:
+            population = None
+        language_raw = find_field(infobox_country, ["language"])
+        neighbours = get_neighbours(country_name, soup_borders)
+
+        data = (
+            country_name,
+            remove_unwanted(capital_raw),
+            population,
+            complete_density(infobox_country),
+            complete_area(infobox_country),
+            remove_unwanted(language_raw),
+            find_field(infobox_country, ["time zone"]),
+            find_field(infobox_country, ["government"]),
+            neighbours
+        )
+
+        cursor = conn.cursor()
+        cursor.execute("""
+        INSERT OR REPLACE INTO countries
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+        """, data)
+        conn.commit()
+        print(f"Successfully added {country_name}!")
+    except Exception as e:
+        print(f"Failed to add {country}: {e}")
+        failed_countries.append(country)
+
+def check_db():
+    conn = sqlite3.connect('countries.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM countries ORDER BY name;")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    conn.close()
+
+def main():
+    conn = init_db()
+    url_borders = "https://en.wikipedia.org/wiki/List_of_countries_and_territories_by_number_of_land_borders"
     req_borders = Request(url_borders, headers={'User-Agent': 'Mozilla/5.0'})
     webpage_borders = urlopen(req_borders).read()
     soup_borders = BeautifulSoup(webpage_borders, 'html.parser')
-    neighbours = get_neighbours(country_name, soup_borders)
+    countries = get_all_countries(soup_borders)
+    for country in countries:
+        try:
+            scrape(country, soup_borders, conn)
+        except Exception as e:
+            print(f"Failed to scrape {country}: {e}")
+    conn.close()
+    check_db()
+    if failed_countries:
+        print(f"Failed to find {len(failed_countries)} countries!")
+        for c in failed_countries:
+            print(c)
 
-    country = {
-        "Name": country_name,
-        "Capital": remove_unwanted(capital_raw),
-        "Population": remove_unwanted(population_raw),
-        "Density": complete_density(infobox_country),
-        "Area": complete_area(infobox_country),
-        "Language": remove_unwanted(language_raw),
-        "Time zone": find_field(infobox_country, ["time zone"]),
-        "Government": find_field(infobox_country, ["government"]),
-        "Neighbours": neighbours
-    }
-    return country
-
-
-def get_country(name_country):
-    template = "https://en.wikipedia.org/wiki/"
-    url = template + name_country
-    url_borders = "https://en.wikipedia.org/wiki/List_of_countries_and_territories_by_number_of_land_borders"
-    info = scrape(url, url_borders)
-    print(info)
-
-
-countries = ["Romania", "Bulgaria", "Indonesia", "Greece", "Turkey", "Japan"]
-for country in countries:
-    get_country(country)
+if __name__ == "__main__":
+    main()
